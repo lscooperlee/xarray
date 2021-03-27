50dbdb43894ba29b1de3b8ef6b3db7b5934cf35e
Cooper Li <davidontech@gmail.com>
2021-03-28 00:24:22 +1300

    Add cmdparser

---
+++ src/cmdparser.h
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <functional>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <cstring>
+
+template <typename... T>
+struct Command
+{
+    Command(
+        std::vector<std::string> keywords,
+        std::string description,
+        std::function<void(T...)> action = [](T...) {})
+        : Keywords(keywords), Description(description), Action(action)
+    {
+    }
+
+    virtual ~Command() = default;
+
+    int NumOfParam = sizeof...(T);
+    std::vector<std::string> Keywords = {};
+    std::string Description = "";
+    std::function<void(T...)> Action = {};
+    std::tuple<T...> Params = {};
+    bool IsSpecified = false;
+};
+
+template <typename T>
+bool ParseCommandOpt(const char *args, T &Commands)
+{
+    int argc = 0;
+    int len = strlen(args);
+    const char *argv[len];
+
+    char arg_copy[len + 1] = {};
+    char *p = arg_copy;
+
+    strcpy(arg_copy, args);
+
+    argv[argc++] = p;
+
+    while (*++p)
+    {
+        if (*p == ' ')
+        {
+            *p = 0;
+        }
+        else if (*p != ' ' && *(p - 1) == 0)
+        {
+            argv[argc++] = p;
+        }
+    };
+
+    return ParseCommandOpt(argc, argv, Commands);
+}
+
+template <typename T>
+bool ParseCommandOpt(int argc, char const *const argv[], T &Commands)
+{
+    int index = 1;
+    bool ret = true;
+
+    auto cmdparser = [&ret, &index, argc, argv](auto &&cmd) {
+        for (int i = index; i < argc; ++i)
+        {
+            auto keyword = argv[i];
+
+            if (std::find(std::cbegin(cmd.Keywords), std::cend(cmd.Keywords), keyword) != std::cend(cmd.Keywords))
+            {
+
+                ++i;
+
+                auto paramparser = [&ret, &i, argv](auto &&param) {
+                    auto cmd_param = argv[i++];
+
+                    if (cmd_param == nullptr)
+                    {
+                        ret = false;
+                        return;
+                    }
+
+                    if (cmd_param[0] != '-')
+                    {
+                        std::istringstream ss(cmd_param);
+                        ss >> param;
+                    }
+                };
+
+                std::apply([paramparser](auto &&... param) { (paramparser(param), ...); },
+                           cmd.Params);
+
+                cmd.IsSpecified = true;
+
+                break;
+            }
+        }
+    };
+
+    std::apply([cmdparser](auto &&... cmd) { (cmdparser(cmd), ...); }, Commands);
+
+    return ret;
+}
+
+//Could use Abbreviated function template in C++20
+template <typename T>
+void RunCommand(T &Commands)
+{
+
+    auto runeach = [](auto &&cmd) {
+        if (cmd.IsSpecified)
+        {
+            std::apply(cmd.Action, cmd.Params);
+        }
+    };
+
+    std::apply([runeach](auto &&... cmd) { (runeach(cmd), ...); }, Commands);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
-int main()
+#include "cmdparser.h"
+#include <iostream>
+
+int main(int argc, const char* const argv[])
 {
+    auto Commands = std::make_tuple(
+        Command<>({ "-h", "--help" }, "help", []() { std::cout << "print help" << std::endl; }));
+
+    ParseCommandOpt(argc, argv, Commands);
+
+    RunCommand(Commands);
+
+    return 0;
 }
